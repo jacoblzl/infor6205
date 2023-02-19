70fe3d4b22354216d9192bb80e9cd3be3701327b
Zhiliang Liu <118701395+jacoblzl@users.noreply.github.com>
2023-02-18 23:44:08 -0500

    Add files via upload

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -17,13 +17,14 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
 
     public static void main(String[] args) {
+        ForkJoinPool Pool = new ForkJoinPool(100);
         processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+        System.out.println("Degree of parallelism: " + Pool.getParallelism());
         Random random = new Random();
         int[] array = new int[2000000];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
+                for (double flag = 0.02; flag <= 1; flag += 0.02 ) {
+            ParSort.cutoff = (int) (array.length * flag);
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
             long time;
             long startTime = System.currentTimeMillis();
@@ -35,6 +36,35 @@ public class Main {
             time = (endTime - startTime);
             timeList.add(time);
 
+
+            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
+
+        }
+//        for (int j = 50; j < 100; j++) {
+//            ParSort.cutoff = 10000 * (j + 1);
+//            // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+//            long time;
+//            long startTime = System.currentTimeMillis();
+//            for (int t = 0; t < 10; t++) {
+//                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+//                ParSort.sort(array, 0, array.length);
+//            }
+//            long endTime = System.currentTimeMillis();
+//            time = (endTime - startTime);
+//            timeList.add(time);
+        for (int thread = 1; thread <= 128; thread *= 2 ) {
+            ParSort.cutoff = (int) (array.length * 0.16);
+            Pool = new ForkJoinPool(thread);
+            // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+            long time;
+            long startTime = System.currentTimeMillis();
+            for (int t = 0; t < 10; t++) {
+                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                ParSort.sort(array, 0, array.length);
+            }
+            long endTime = System.currentTimeMillis();
+            time = (endTime - startTime);
+            timeList.add(time);
 
             System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
 
