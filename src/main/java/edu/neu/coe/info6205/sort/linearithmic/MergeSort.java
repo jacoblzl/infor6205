df4cb639c695ba7f76554932f590b966a5ca9e8e
jacoblzl <zhiliangliu0728@gmail.com>
2023-03-09 18:07:55 -0500

    Adds zhiliang liu

--- src/main/java/edu/neu/coe/info6205/sort/linearithmic/MergeSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/linearithmic/MergeSort.java
@@ -60,6 +60,36 @@ public class MergeSort<X extends Comparable<X>> extends SortWithHelper<X> {
         }
 
         // FIXME : implement merge sort with insurance and no-copy optimizations
+        if(from >= to) return;
+        int mid = (to-from)/2+from;
+        if(noCopy) {
+
+            sort(aux, a, from, mid);
+            sort(aux, a, mid, to);
+
+            // Insurance
+            if(insurance && helper.less(aux, mid - 1, mid)) {
+                for(int k = from; k < to; k++) {
+                    a[k] = aux[k];
+                }
+                return;
+            }
+
+            // Implement merge
+            merge(aux, a, from, mid, to);
+        }
+        else {
+            sort(a, aux, from, mid);
+            sort(a, aux, mid, to);
+
+            // Insurance
+            if (insurance && helper.less(a[mid - 1], a[mid])) return;
+
+            // Copy
+            for (int k = from; k < to; k++) aux[k] = a[k];
+            // Implement merge
+            merge(aux, a, from, mid, to);
+        }
         // END 
     }
 
